2fee1059da85bc480160b70a438d5294ae54f7a6
Harsh Mohan <harshmohan813@gmail.com>
2021-02-23 13:06:02 +0000

    Created the parked blog front page

---
+++ Learning/Gatsby/firstLook/src/pages/blogs.js
@@ -0,0 +1,19 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Header from "../components/header"
+import Preview from "../components/preview"
+
+
+import "../styles/layout.css"
+import "../styles/styles.scss"
+import "../styles/blogs.scss"
+
+const Blogs = () => (
+  <>
+    <Header />
+    <Preview />
+  </>
+)
+
+export default Blogs

4154ca74b1d21cebeafe067cca849e51db12078f
Harsh Mohan <harshmohan813@gmail.com>
2021-02-26 17:55:11 +0000

    Going to change the css

--- Learning/Gatsby/firstLook/src/components/blogTemplate.js
+++ Learning/Gatsby/firstLook/src/components/blogTemplate.js
@@ -21,30 +21,33 @@ const BlogTemplate = () => {
     <div className="blogpage">
       <div className="container">
         <div className="row">
-          <div className="main-image">
+          {/* <div className="main-image">
             <Img fluid={data.poppinShades.childImageSharp.fluid}
             style={{maxHeight: '100%'}}/>
           </div>
-          <div className="side-arrow left">
-
-          </div>
+          <a class="arrow arrow-left" title="Previous" href="javascript:;"></a>
           <div className="main-text">
             Ayy Lmao
           </div>
+          <a class="arrow arrow-right" title="Previous" href="javascript:;"></a>
 
+          <div className="author">
+            Harsh Mohan
+          </div> */}
 
-          <div className="side-arrow right">
-
-          </div>
         </div>
 
-        <p>
-          hi this is my new blog
-          hi this is my new blog
-          hi this is my new blog
-          hi this is my new blog
-          hi this is my new blog
-        </p>
+        <div className="content">
+          <div className="inner-content">
+            <p>
+              hi this is my new blog {"\n"}
+              hi this is my new blog
+              hi this is my new blog
+              hi this is my new blog
+              hi this is my new blog
+            </p>
+          </div>
+        </div>
       </div>
 
     </div>

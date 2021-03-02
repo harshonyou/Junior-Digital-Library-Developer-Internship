6862c4b8d18a6ed248be986dfc15508cbfb2a28e
Sebastiaan Schoonebeek <sebastiaan@websnacks.nl>
2021-03-02 15:05:17 +0000

    Added an example on how to use createPages

--- Learning/Gatsby/firstLook/gatsby-node.js
+++ Learning/Gatsby/firstLook/gatsby-node.js
@@ -4,4 +4,66 @@
  * See: https://www.gatsbyjs.com/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+
+// I use axios to fetch the JSON
+const axios = require('axios')
+
+// Just a basic JS function for colling the API
+const callApi = () => axios.get(`https://jsonplaceholder.typicode.com/posts/`)
+
+// This function is called within the `createPages` function and will return the JSON
+const fetchPosts = async () => {
+    return await callApi()
+};
+
+// just a function to transform any string to a URL friendly string
+function slugify(string, spacer = '-') {
+    const a = 'àáäâãåăæąçćčđďèéěėëêęğǵḧìíïîįłḿǹńňñòóöôœøṕŕřßşśšșťțùúüûǘůűūųẃẍÿýźžż·/_,:;'
+    const b = 'aaaaaaaaacccddeeeeeeegghiiiiilmnnnnooooooprrsssssttuuuuuuuuuwxyyzzz------'
+    const p = new RegExp(a.split('').join('|'), 'g')
+
+    return string.toString().toLowerCase()
+        .replace(/\s+/g, spacer) // Replace spaces with -
+        .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
+        .replace(/&/g, '-and-') // Replace & with 'and'
+        .replace(/[^\w-]+/g, '') // Remove all non-word characters
+        .replace(/--+/g, spacer) // Replace multiple - with single -
+        .replace(/-+$/, '') // Trim - from end of text
+}
+
+// Okay lets create some pages :)
+exports.createPages = async ({ actions: { createPage } }) => {
+
+    // First we need to call the API and get the posts
+    const posts = await fetchPosts()
+
+    if (posts.length > 0) {
+
+        // We map through the posts
+        posts.map((post) => {
+
+            // We create an array with possible URL's for each post
+            const postPaths = [
+                "/posts/" + slugify(post.id) + "/" + slugify(post.title),
+                "/posts/" + slugify(post.id)
+            ]
+
+            console.log('postPaths');
+            console.log(postPaths);
+
+            // Then we loop through the possible URL's and create the pages. 
+            // createPage takes 3 arugments:
+            // 1 - url (path)
+            // 2 - a template
+            // 3 - the post itself which you can you use on the template
+            postPaths.map(postPath => {
+                createPage({
+                    path: postPath,
+                    component: require.resolve('./src/templates/postTemplate.js'),
+                    context: { post }
+                })
+            })
+
+        })
+    }
+}

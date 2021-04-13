a17c1bb1e9be712eb1e4b2b78cf4bd75873846d6
Harsh Mohan <harshmohan813@gmail.com>
2021-04-13 13:28:45 +0100

    Started Creating Plugins

--- Storyline Features Component/storyline-features/src/pages/Story/storyRequest.js
+++ Storyline Features Component/storyline-features/src/pages/Story/storyRequest.js
@@ -1,8 +1,19 @@
+import { makeStyles } from '@material-ui/core'
 import React from 'react'
 import RequestForm from '../../components/Story/requestForm'
 
+const useStyles = makeStyles(theme => ({
+    root:{
+        width: '80%',
+        margin: theme.spacing(1),
+        backgroundColor: 'grey'
+    }
+}))
+
 export default function StoryRequest() {
+    const classes = useStyles();
     return (
-        <RequestForm/>
+        <RequestForm className={classes.root}/>
     )
 }
+ 

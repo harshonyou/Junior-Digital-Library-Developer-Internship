fca913800e286c09dd8526e0660e07855e48e8bb
Harsh Mohan <harshmohan813@gmail.com>
2021-04-21 14:19:43 +0100

    Presentation Demo Done

---
+++ Storyline Features Component/editorjs-plugins/custom-image/data-blog-general.js
@@ -0,0 +1,81 @@
+data_blog_general = {
+    "time": 1619008990473,
+    "blocks": [
+        {
+            "type": "header",
+            "data": {
+                "text": "<b>Blog Page</b>",
+                "level": 2
+            },
+            "tunes": {
+                "anyTuneName": {
+                    "alignment": "left"
+                }
+            }
+        },
+        {
+            "type": "header",
+            "data": {
+                "text": "This is the <b>HEADER</b>",
+                "level": 2
+            },
+            "tunes": {
+                "anyTuneName": {
+                    "alignment": "center"
+                }
+            }
+        },
+        {
+            "type": "paragraph",
+            "data": {
+                "text": "Welcome to the template for <i><b>Blog</b> Story Telling.</i>"
+            },
+            "tunes": {
+                "anyTuneName": {
+                    "alignment": "left"
+                }
+            }
+        },
+        {
+            "type": "image",
+            "data": {
+                "url": "https://i.ibb.co/PDP7PMV/image.png",
+                "caption": "  Every Block is an Individual Box, which can have various functions to it.",
+                "withBorder": false,
+                "withBackground": false,
+                "stretched": false
+            }
+        },
+        {
+            "type": "image",
+            "data": {
+                "url": "https://i.ibb.co/vvXN1xP/image.png",
+                "caption": "You may find various options to deal with once you hit \"Add Option\"",
+                "withBorder": false,
+                "withBackground": false,
+                "stretched": false
+            }
+        },
+        {
+            "type": "image",
+            "data": {
+                "url": "https://i.ibb.co/ZgZnQBn/image.png",
+                "caption": "You may find more settings for each feature once you hit \"Click to tune\"",
+                "withBorder": false,
+                "withBackground": false,
+                "stretched": false
+            }
+        },
+        {
+            "type": "image",
+            "data": {
+                "url": "https://i.ibb.co/sCP0kJx/image.png",
+                "caption": "You may find general options for the whole editor in the end.",
+                "withBorder": false,
+                "withBackground": false,
+                "stretched": false
+            }
+        }
+    ],
+    "version": "2.20.2"
+}
--- Storyline Features Component/editorjs-plugins/custom-image/text-over-image.js
+++ Storyline Features Component/editorjs-plugins/custom-image/text-over-image.js
@@ -2,7 +2,7 @@ class TextOverImage{
 
     static get toolbox() {
         return {
-            title: 'Text Over Image',
+            title: 'Parallax',
             icon: '<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24" x="0"/></g><g><path d="M22.47,5.2C22,4.96,21.51,4.76,21,4.59v12.03C19.86,16.21,18.69,16,17.5,16c-1.9,0-3.78,0.54-5.5,1.58V5.48 C10.38,4.55,8.51,4,6.5,4C4.71,4,3.02,4.44,1.53,5.2C1.2,5.36,1,5.71,1,6.08v12.08c0,0.58,0.47,0.99,1,0.99 c0.16,0,0.32-0.04,0.48-0.12C3.69,18.4,5.05,18,6.5,18c2.07,0,3.98,0.82,5.5,2c1.52-1.18,3.43-2,5.5-2c1.45,0,2.81,0.4,4.02,1.04 c0.16,0.08,0.32,0.12,0.48,0.12c0.52,0,1-0.41,1-0.99V6.08C23,5.71,22.8,5.36,22.47,5.2z M10,16.62C8.86,16.21,7.69,16,6.5,16 c-1.19,0-2.36,0.21-3.5,0.62V6.71C4.11,6.24,5.28,6,6.5,6C7.7,6,8.89,6.25,10,6.72V16.62z M19,0.5l-5,5V15l5-4.5V0.5z"/></g></svg>'
         };
     }
@@ -41,8 +41,14 @@ class TextOverImage{
         this.wrapper.classList.add('text-over-image');
 
         if (this.data && this.data.url){
-            this._createImage(this.data.url, this.data.caption);
+            this._createImage(this.data.url, this.data.caption, this.data.alt);
             return this.wrapper;
+        }
+        
+        if (this.data && this.data.toggle){
+            this.data.left = false;
+            this.data.right = true;
+            this._acceptTuneView();
         }
 
         const input = document.createElement('input');
@@ -57,7 +63,7 @@ class TextOverImage{
         return this.wrapper;
     }
 
-    _createImage(url, captionText){
+    _createImage(url, captionText, altText){
         const container = document.createElement('div')
         
         const image = document.createElement('img');
@@ -74,7 +80,8 @@ class TextOverImage{
 
         text.classList.add('input');
         text.contentEditable = true;
-        text.innerHTML = captionText || '';
+        text.innerHTML = altText || '';
+        text.setAttribute("data-placeholder", "Description...");
 
         container.appendChild(image)
         container.appendChild(text)
